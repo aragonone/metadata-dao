93eb84a5a33de131369d7df2fde621fbfd1e09b5
Jorge Izquierdo <jorge@izqui.me>
2019-02-13 19:56:23 +0100

    Fix radspec indexer's signatures and mplement new names view

--- data/radspec/indexer.js
+++ data/radspec/indexer.js
@@ -1,13 +1,22 @@
 const IndexLib = require('../../../lib/index-lib') // This will be executed inside build/raw/[dbName]
+const { keccak256 } = require('js-sha3')
+
+const sigBytes = signature => `0x${keccak256(signature).slice(0, 8)}`
 
 const index = [
   {
     key: '@key',
     api: 'sigs',
-    pathTransform: item => '0x12345678', // TODO
+    pathTransform: item => sigBytes(item['@key']),
     dataTransform: item => item['@value'],
     dumpTransform: text => text,
     extension: ''
+  },
+  {
+    key: '@key',
+    api: 'name',
+    pathTransform: item => item['@key'],
+    dataTransform: item => ({ sig: sigBytes(item['@key']), notice: item['@value'] }),
   }
 ]
 
