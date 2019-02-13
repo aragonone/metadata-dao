b7e62c3872ab2a0c574aa2bd65e9ee4dab63a3da
Jorge Izquierdo <jorge@izqui.me>
2019-02-13 23:18:44 +0100

    Update radspec sig view

--- data/radspec/indexer.js
+++ data/radspec/indexer.js
@@ -5,12 +5,10 @@ const sigBytes = signature => `0x${keccak256(signature).slice(0, 8)}`
 
 const views = [
   {
-    api: 'sigs', // on the /api/radspec/sigs/:sig resource
+    api: 'sig', // on the /api/radspec/sig/:sig resource
     key: '@key', // index by the key of the object (the function signature)
     pathTransform: item => sigBytes(item['@key']), // paths are the signature identifier /api/radspec/sigs/0x12345678
-    dataTransform: item => item['@value'], // the served data is the value of the object (the radspec string)
-    dumpTransform: text => text, // when dumping to text, don't transform it
-    extension: '' // resource has no extension (plain text)
+    dataTransform: item => ({ signature: item['@key'], notice: item['@value'] }), // the served data is a JSON object
   },
   {
     api: 'name', // on the /api/radspec/name/:name resource
